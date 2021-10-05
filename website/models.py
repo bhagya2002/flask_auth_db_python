b6a9cb1286fab121165a9d1dbdc16b280f064a62
bhagya2002 <bhagya02.p@gmail.com>
2021-10-04 23:26:57 -0600

    updates model and extra info

--- website/models.py
+++ website/models.py
@@ -3,13 +3,13 @@ from flask_login import UserMixin # custom user class to inherit from
 from sqlalchemy.sql import func # import the func function from sqlalchemy and gives default values by itself
 
 
-class Note(db.model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # foreign key to user table to get the user id of who made the note
 
-class User(db.model, UserMixin): # create a new class to inherit from the UserMixin class
+class User(db.Model, UserMixin): # create a new class to inherit from the UserMixin class
     id = db.Column(db.Integer, primary_key=True) # create a new column called id with type Integer and primary key
     email = db.Column(db.String(150), unique=True, nullable=False) # create a new column called email with type String and unique and has to be filled
     password = db.Column(db.String(150), nullable=False) # create a new column called password with type String and has to be filled
